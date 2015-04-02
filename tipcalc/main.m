32a803b46caca99fba138daf4f6dc1ddc341603a
zemos <zemos@zemoss-Mac-mini.zemonet>
2015-04-01 21:00:24 -0700

    Initial Commit

---
+++ tipcalc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tipcalc
+//
+//  Created by zemos on 4/1/15.
+//  Copyright (c) 2015 tipcalc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
